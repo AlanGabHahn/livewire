fb3dca9f2126f99a12b67d18438b42399c634ece
Alan <alan_hahn1995@outlook.com>
2024-07-24 11:06:33 -0300

    Criar Componentes e Data Binding

---
+++ resources/views/livewire/show-tweets.blade.php
@@ -0,0 +1,5 @@
+<div>
+    If your happiness depends on money, you will never be happy with yourself.
+    <p>{{ $message }}</p>
+    <input type="text" name="message" id="message" wire:model="message">
+</div>
